174281d134e8d62860a712d842663eeadd52eece
Administrator <Administrator@SERVER2>
2025-07-29 16:29:22 +0300

    1

---
+++ HideAndSeek/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideAndSeek
+{
+    internal class GameController
+    {
+        public void Status() { }//stub
+        public string ParseInput(string input) { return ""; }//stub
+        public void Prompt() { }//stub
+    }
+}
--- HideAndSeek/Program.cs
+++ HideAndSeek/Program.cs
@@ -2,8 +2,31 @@
 {
     internal class Program
     {
+        private string DescribeDirection(Direction d)
+            {
+                switch (d)
+                {
+                    case Direction.Up:
+                        return "Up";
+                    case Direction.Down:
+                        return "Down";
+                    case Direction.In:
+                        return "In";
+                    case Direction.Out:
+                        return "Out";
+                    default:
+                        return $"to the {d}";
+                }
+            }
         static void Main(string[] args)
         {
+            GameController gameController = new GameController();
+            while (true)
+            {
+                Console.WriteLine(gameController.Status);
+                Console.Write(gameController.Prompt);
+                Console.WriteLine(gameController.ParseInput(Console.ReadLine()));
+            }
             Console.WriteLine("Hello, World!");
         }
     }
